f724ca10d0d7ce71d3e7170a061b870d71e9f88c
kevinli2025 <126900859+kevinli2025@users.noreply.github.com>
2023-03-15 09:45:25 -0700

    first commit

---
+++ settings.py
@@ -0,0 +1,19 @@
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50, 50, 255)
+RED = (255, 50, 50)
+
+# mob attributes
+MOB_ACC = 2
---
+++ sprites.py
@@ -0,0 +1,87 @@
+# File created by: Kevin Li
+
+import pygame as pg
+
+from pygame.sprite import Sprite
+
+from settings import *
+
+from random import randint
+
+vec = pg.math.Vector2
+
+# create a player
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+    def update(self):
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc 
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+        if self.rect.x > WIDTH:
+            print("I'm off the right screen...")
+        if self.rect.x < 0:
+            print("I'm off the left screen...")
+        if self.rect.y < 0:
+            print("I'm off the top screen...")
+        if self.rect.y > HEIGHT:
+            print("I'm off the bottom screen...")
+
+class Mob(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def behavior(self):
+        self.pos.x += self.vel.x
+        self.pos.y += self.vel.y
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_w]:
+            self.acc.y = -MOB_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -MOB_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = MOB_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = MOB_ACC
+    def update(self):
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+        if self.rect.x > WIDTH:
+            print("I'm off the right screen...")
+        if self.rect.x < 0:
+            print("I'm off the left screen...")
+        if self.rect.y < 0:
+            print("I'm off the top screen...")
+        if self.rect.y > HEIGHT:
+            print("I'm off the bottom screen...")
