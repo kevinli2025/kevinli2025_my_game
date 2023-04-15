c3439eedeb7b3c2385b04799487ff73c9287bb32
kevinli2025 <126900859+kevinli2025@users.noreply.github.com>
2023-04-15 14:50:16 -0700

    initial check point

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
-# File created by: Kevin Li
+# File created by: Chris Cozort
 
+
+# import libs
 import pygame as pg
-import random
 import os
-
-# need this to create file paths effectively
-from os import path
-
-# import settings
+# import settings 
 from settings import *
 from sprites import *
-from random import randint
 # from pg.sprite import Sprite
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "images")
+img_folder = os.path.join(game_folder, "img")
 
-# create the game class to organize game content better...
+# create game class in order to pass properties to the sprites file
+
 class Game:
-    # inits the pygame stuff including setting up screen/display area
     def __init__(self):
+        # init game window etc.
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("My Game...")
+        pg.display.set_caption("my game")
         self.clock = pg.time.Clock()
         self.running = True
-    # method to create a new game...
+        print(self.screen)
     def new(self):
-            self.score = 0
-            self.all_sprites = pg.sprite.Group()
-            self.platforms = pg.sprite.Group()
-            self.enemies = pg.sprite.Group()
-            # instantiates player class from sprites file, and passes this game class as
-            # an argument
-            self.player = Player(self)
-            self.all_sprites.add(self.player)
-            for i in range(1,10):
-                e = Mob()
-                self.all_sprites.add(e)
-            self.run()  
+        # starting a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        # self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        for i in range(0,10):
+            m = Mob(20,20,(0,255,0))
+            self.all_sprites.add(m)
+            self.enemies.add(m)
+        self.run()
     def run(self):
         self.playing = True
         while self.playing:
@@ -48,10 +55,9 @@ class Game:
             self.events()
             self.update()
             self.draw()
+    
     def events(self):
-        # Game Loop - events
         for event in pg.event.get():
-            # check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
@@ -59,30 +65,42 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump()
+    def update(self):
+        self.all_sprites.update()
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+
+    def draw(self):
+        self.screen.fill(BLUE)
+        self.draw_text("hello there...", 24, WHITE, WIDTH/2, HEIGHT/2)
+        self.all_sprites.draw(self.screen)
+
+        # is this a method or a function?
+        pg.display.flip()
     def draw_text(self, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
+        text_rect.midtop = (x,y)
         self.screen.blit(text_surface, text_rect)
-    # def get_mouse_now():
-    #     x,y = pg.mouse.get_pos()
-    #     return (x,y)
-    def update(self):
-        self.all_sprites.update()
-    def draw(self):
-        self.screen.fill(BLUE)
-        self.draw_text("Hello there!", 42, WHITE, WIDTH/2, HEIGHT/10)
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)
 
-vec = pg.math.Vector2
-
-# game loop
-
+# instantiate the game class...
 g = Game()
 
+# kick off the game loop
 while g.running:
     g.new()
 
---
+++ test.py
@@ -0,0 +1,126 @@
+import pygame
+import math
+from collections import deque
+import os
+
+# Game Constants
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+MAX_BOUNCES = 3
+MAX_BULLETS = 5
+
+game_folder = os.path.dirname(__file__)
+
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0,255,0)
+
+# Bullet Class
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, pos, angle, *groups):
+        super().__init__(*groups)
+        self.image = pygame.Surface((4, 4))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = 10
+        self.vx = math.cos(math.radians(angle)) * self.speed
+        self.vy = math.sin(math.radians(angle)) * self.speed
+        self.bounces = 0
+
+    def update(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.vx = -self.vx
+            self.bounces += 1
+
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.vy = -self.vy
+            self.bounces += 1
+
+        if self.bounces > MAX_BOUNCES:
+            self.kill()
+# Player Class
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, *groups):
+        super().__init__(*groups)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect(center=pos)
+
+    def update(self, mouse_pos):
+        angle = math.degrees(math.atan2(mouse_pos[1] - self.rect.centery, mouse_pos[0] - self.rect.centerx))
+        self.image = pygame.transform.rotate(self.original_image, -angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+# Enemy Class
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos, *groups):
+        super().__init__(*groups)
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect(center=pos)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Ricochet Game")
+    clock = pygame.time.Clock()
+
+
+
+    # Game variables
+    bullets = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    bullets_queue = deque(maxlen=MAX_BULLETS)
+
+    player_pos = (0, HEIGHT)
+    players = pygame.sprite.Group()
+    player = Player(player_pos,players)
+
+
+    # Create some enemies
+    for i in range(10):
+        enemy = Enemy((i * 50 + 50, 50), enemies)
+
+    running = True
+    while running:
+        screen.fill(BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if len(bullets) < MAX_BULLETS:
+                    mouse_pos = pygame.mouse.get_pos()
+                    angle = math.degrees(math.atan2(mouse_pos[1] - player_pos[1], mouse_pos[0] - player_pos[0]))
+                    bullet = Bullet(player_pos, angle, bullets)
+                    bullets_queue.append(bullet)
+                else:
+                    oldest_bullet = bullets_queue.popleft()
+                    oldest_bullet.kill()
+
+        # Update
+        bullets.update()
+        bullets.draw(screen)
+        enemies.draw(screen)
+
+        # Check for collisions
+        for bullet in bullets:
+            enemy_hit = pygame.sprite.spritecollide(bullet, enemies, True)
+            if enemy_hit:
+                bullet.kill()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+
